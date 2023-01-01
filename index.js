709d7fdb1b4efb7eac1b5ddc1e8fc3fc1e36c0f6
David <dmaine6711@gmail.com>
2022-12-31 19:02:31 -0500

    test comment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function elliotsOrder() {
     let word1 = array1[Math.floor(Math.random() * 10)];
     let word2 = array2[Math.floor(Math.random() * 10)];
     let word3 = array3[Math.floor(Math.random() * 10)];
-
+  //yas
     return `Elliot would like to order ${word1} ${word2} cooked ${word3}`;
 }
 
